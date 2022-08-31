b767f2ab78a5bc95275d4f54567dc441f83e59bb
youname <1547058501@qq.com>
2022-08-31 23:47:16 +0800

    submit project

---
+++ signin/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import elementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '../src/assets/zhoujiazhen.css'
+
+createApp(App).use(store).use(elementPlus).use(router).mount('#app')
